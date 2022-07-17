c6364fe1201b2e21be5a6d9b58870209041039a1
haoqiusong <106828678+haoqiusong@users.noreply.github.com>
2022-07-17 17:56:56 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="virseqimprover",
+    version="1.0",
+    author="Haoqiu Song",
+    author_email="haoqiu@vt.edu",
+    description="An integrated pipeline for error-correction, extension, and annotation of viral scaffolds.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/haoqiusong/VirSeqImprover",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ),
+)
